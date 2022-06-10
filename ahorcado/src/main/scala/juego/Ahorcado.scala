8ffc6283c048d1d41ff47484534d6f21374a07df
sergio dominguez <sergio.dominguezsoto.st@everis.nttdata.com>
2022-06-10 11:54:23 +0200

    Update Ahorcado.scala

--- ahorcado/src/main/scala/juego/Ahorcado.scala
+++ ahorcado/src/main/scala/juego/Ahorcado.scala
@@ -3,147 +3,147 @@ package juego
 import juegos.Utils
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import scala.util.matching.Regex
 import scala.io.StdIn.readLine
 
 
-class Ahorcado {
+object juego {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark: SparkSession = SparkSession.builder()
+      .master("local")
+      .appName("prueba")
+      .getOrCreate();
+
+    val direccionCSV = "C:\\Users\\sdominso\\Documents\\GitHub\\ahorcado\\src\\main\\resources\\words.csv"
 
 
-  object juego {
-    def main(args: Array[String]): Unit = {
-      Logger.getLogger("org").setLevel(Level.ERROR)
-      val spark: SparkSession = SparkSession.builder()
-        .master("local")
-        .appName("prueba")
-        .getOrCreate();
+    val listaPalabras = Utils.ingestCSV(direccionCSV, spark, ";")
+      .select("ID", "PALABRA").rdd.map(row => (row(0), row(1)))
+      .collect().toList.asInstanceOf[List[(Int, String)]];
 
-      val direccionCSV = ""
+    var volverAJugar = true
+    do {
+      // es la 1a vez que juega o repite:
+      var partida = inicializarPartida(listaPalabras)
+      if (siguienteTurno(partida, dibujos, listaPalabras, 0)) {
+        // gano
+        println("Ganaste!!")
+      } else {
+        // perdio
+        print(s"Fallaste!! La palabra era '${partida.palabraString}''")
+      }
+      println("¿Echamos otra? Y/N")
 
-
-      val listaPalabras = Utils.ingestCSV(direccionCSV, spark, ";")
-        .select("ID", "PALABRA").rdd.map(row => (row(0), row(1)))
-        .collect().toList.asInstanceOf[List[(Int, String)]];
-
-      var volverAJugar = true
-      do {
-        // es la 1a vez que juega o repite:
-        var partida = inicializarPartida(listaPalabras)
-        if (siguienteTurno(partida, dibujos, listaPalabras)) {
-          // gano
-          println("Ganaste!!")
-        } else {
-          // perdio
-          print(s"Fallaste!! La palabra era '${partida.palabraString}''")
-        }
-        println("¿Echamos otra? Y/N")
-
-        var respuesta = readLine()
-        while (!respuesta.equals("Y") ||
-          !respuesta.equals("y") ||
-          !respuesta.equals("N") ||
-          !respuesta.equals("n")) {
-          print("Escriba 'Y' o 'N' por favor")
-        }
-        if (respuesta.equals("Y") ||
-          respuesta.equals("y")) {
-          volverAJugar = true
-        } else {
-          volverAJugar = false
-        }
-      } while (volverAJugar)
-      // ha acabado y no quiere volver a jugar
-      System.exit(0)
-    }
-
-
-    var dibujos: Array[String] = new Array[String](10)
-
-    dibujos(0) =
-      ""
-    dibujos(1) =
-      "________||______"
-    dibujos(2) =
-      "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(3) =
-      "__________\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(4) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(5) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "   ◯    ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(6) =
-      "__________\\n\"" +
-        "   |    ||\\n\"" +
-        "   |    ||\\n\"" +
-        "   ◯    ||\\n\"" +
-        "   |    ||\\n\"" +
-        "   |    ||\\n\"" +
-        "        ||\\n\"" +
-        "________||______"
-    dibujos(7) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "   ◯    ||\n" +
-        "  /|    ||\n" +
-        "   |    ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(8) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "   ◯    ||\n" +
-        "  /|\\   ||\n" +
-        "   |    ||\n" +
-        "        ||\n" +
-        "________||______"
-    dibujos(9) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "   ◯    ||\n" +
-        "  /|\\   ||\n" +
-        "   |    ||\n" +
-        "  /     ||\n" +
-        "________||______"
-    dibujos(10) =
-      "__________\n" +
-        "   |    ||\n" +
-        "   |    ||\n" +
-        "   ◯    ||\n" +
-        "  /|\\   ||\n" +
-        "   |    ||\n" +
-        "  / \\   ||\n" +
-        "________||______"
-
+      var respuesta = readLine()
+      while (!respuesta.equals("Y") &&
+        !respuesta.equals("y") &&
+        !respuesta.equals("N") &&
+        !respuesta.equals("n")) {
+        print("Escriba 'Y' o 'N' por favor")
+        respuesta = readLine()
+      }
+      if (respuesta.equals("Y") ||
+        respuesta.equals("y")) {
+        volverAJugar = true
+      } else {
+        volverAJugar = false
+      }
+    } while (volverAJugar)
+    // ha acabado y no quiere volver a jugar
+    System.exit(0)
   }
+
+
+  var dibujos: Array[String] = new Array[String](11)
+
+  dibujos(0) =
+    ""
+  dibujos(1) =
+    "________||______"
+  dibujos(2) =
+    "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(3) =
+    "__________\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(4) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(5) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(6) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(7) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "  /|    ||\n" +
+      "   |    ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(8) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "  /|\\   ||\n" +
+      "   |    ||\n" +
+      "        ||\n" +
+      "________||______"
+  dibujos(9) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "  /|\\   ||\n" +
+      "   |    ||\n" +
+      "  /     ||\n" +
+      "________||______"
+
+
+  dibujos(10) =
+    "__________\n" +
+      "   |    ||\n" +
+      "   |    ||\n" +
+      "   ◯    ||\n" +
+      "  /|\\   ||\n" +
+      "   |    ||\n" +
+      "  / \\   ||\n" +
+      "________||______"
+
 
   class Partida {
     var palabraString: String = ""
@@ -155,7 +155,7 @@ class Ahorcado {
 
     def inicializarPartida(palabra: String, intentosMax: Int, estadoRespuesta: Array[Char]): Unit = {
       this.palabraString = palabra
-      var palabraArray = palabra.toCharArray
+      this.palabraArray = palabra.toCharArray
       this.intentosMax = intentosMax
       this.estadoRespuesta = estadoRespuesta
     }
@@ -165,19 +165,18 @@ class Ahorcado {
     var partida = new Partida
 
     // inicializo las variables base para nueva partida
-    val intentosMax = 5
+    val intentosMax = dibujos.length
     val simboloIncognita = '_'
     //
     val palabraNum = scala.util.Random.nextInt(listaPalabras.length - 1)
-    val palabra = listaPalabras(palabraNum)._2
+    val palabra = listaPalabras(palabraNum)._2.toLowerCase
     var estadoRespuesta = new Array[Char](palabra.length)
-    for (i <- estadoRespuesta.indices) {
+    for (i <- 0 to estadoRespuesta.indices.length - 1) {
       estadoRespuesta(i) = simboloIncognita
     }
     partida.inicializarPartida(palabra, intentosMax, estadoRespuesta)
     // ya tengo el objeto "partida" inicializado
     partida
-
   }
 
 
@@ -192,19 +191,22 @@ class Ahorcado {
 
     //
     println(dibujo)
-
+    print("\n")
     estadoRespuesta.foreach(e => {
       print(e)
     })
-
+    println("\n")
     println(s"Nº de errores: ${intentosRestantes}/${maximoIntentos}\n")
 
-    print("\nLetras utilizadas: [")
-    for (i <- 0 to historialRespuestas.length - 2) { // pinto todas las letras menos la ultima
-      print("${historialRespuestas(i)}, ")
+    if (historialRespuestas.length > 0) {
+      print("Letras utilizadas: [")
+      for (i <- 0 to historialRespuestas.length - 2) { // pinto todas las letras menos la ultima
+        print(s"${historialRespuestas(i)}, ")
+      }
+      print(historialRespuestas(historialRespuestas.length - 1)) // pinto la ultima letra
+      println("]")
     }
-    print(historialRespuestas(historialRespuestas.length - 1)) // pinto la ultima letra
-    println("]")
+
     println("Introduce letra")
   }
 
@@ -214,7 +216,7 @@ class Ahorcado {
   def comprobarRespuestaEnHistorial(historialRespuestas: Array[Char], respuesta: Char): Boolean = {
     historialRespuestas.foreach(e => {
       if (e.equals(respuesta)) {
-        true
+        return true
       }
     })
     false
@@ -227,34 +229,35 @@ class Ahorcado {
    * mas eficiente seria con un arbol rojonegro
    */
   def ordenarRespuestas(historialRespuestas: Array[Char], respuesta: Char): Array[Char] = {
+    println(s"ordenarRespuestas, respuesta: ${respuesta}, como int: ${respuesta.asInstanceOf[Int]}")
     val respuestasOrdenadas = new Array[Char](historialRespuestas.length + 1)
 
+    /*
     // en caso de que el array historial este vacio
     if (historialRespuestas.length == 0) {
       respuestasOrdenadas(0) = respuesta
       return respuestasOrdenadas
     }
+    */
+
 
     var ite = 0
-    for (i <- 0 to historialRespuestas.length - 2) {
-      if (historialRespuestas(i) < respuesta && respuesta < historialRespuestas(i + 1)) {
-        // bingo
+    var respuestaMetida = false
+    for (i <- 0 to historialRespuestas.length - 1) {
+      if (historialRespuestas(i) <= respuesta) { // no se mete la respuesta
         respuestasOrdenadas(ite) = historialRespuestas(i)
-        ite += 1
+      } else { // bingo, se mete la respuesta
+        respuestasOrdenadas(ite) = respuesta
+        respuestaMetida = true
       }
-      respuestasOrdenadas(ite) = respuesta
       ite += 1
     }
-
-    // por si hay que meterlo al final del array o una antes
-    if (historialRespuestas(historialRespuestas.length - 1) < respuesta) {
-      // al final
+    if (!respuestaMetida) { // caso que se da si la respuesta hay que meterla al final
       respuestasOrdenadas(respuestasOrdenadas.length - 1) = respuesta
-      respuestasOrdenadas(respuestasOrdenadas.length - 2) = historialRespuestas(historialRespuestas.length - 1)
-    } else {
-      // una antes
-      respuestasOrdenadas(respuestasOrdenadas.length - 2) = respuesta
-      respuestasOrdenadas(respuestasOrdenadas.length - 1) = historialRespuestas(historialRespuestas.length - 1)
+    }
+    if (respuestasOrdenadas.length >= 3) {
+      println(s"char at [2]: ${respuestasOrdenadas(2).asInstanceOf[Int]}")
+
     }
     respuestasOrdenadas
   }
@@ -264,43 +267,31 @@ class Ahorcado {
    * si pierde, retorna false
    * si aun queda por jugar, caso recursivo
    */
-  def siguienteTurno(partida: Partida, dibujos: Array[String], listaPalabras: List[(Int, String)]): Boolean = {
+  def siguienteTurno(partida: Partida, dibujos: Array[String], listaPalabras: List[(Int, String)], turno: Int): Boolean = {
     // escribo
     imprimirPartida(dibujos(partida.intentos), partida.estadoRespuesta,
       partida.historialRespuestas, partida.intentos, partida.intentosMax)
 
-    // leo
-    var respuesta = readLine()
-    var inputOk = true
-    val letra = respuesta.charAt(0)
-    do {
-      var inputOk = true
-      if (respuesta.length > 1) {
-        println("Solo una letra, por favor")
-        respuesta = readLine()
-        inputOk = false
-      }
-      if ((letra < 'A' || letra > 'Z') &&
-        (letra < 'a' || letra > 'z')) {
-        println("Introduzca una letra, no un símbolo, por favor")
-        respuesta = readLine()
-        inputOk = false
-      }
-    } while (!inputOk)
+    // leo entrada
+    var respuesta:String = leerEntrada()
+    while (comprobarRespuestaEnHistorial(partida.historialRespuestas, respuesta.toLowerCase.charAt(0))) {
+      println("Ya ha introducido esa letra")
+      respuesta = leerEntrada()
+    }
 
-
+    val letra = respuesta.toLowerCase.charAt(0)
     //actualizo la partida
     var acerto = false
-    for (i <- partida.palabraArray) {
+    for (i <- 0 to partida.palabraArray.length - 1) {
       if (partida.palabraArray(i).equals(letra)) {
         acerto = true
         partida.estadoRespuesta(i) = letra
       }
-      if (!acerto) {
-        partida.intentos += 1
-      }
-      partida.historialRespuestas = ordenarRespuestas(partida.historialRespuestas, letra)
     }
+    if (!acerto) {
+      partida.intentos += 1
+    }
+    partida.historialRespuestas = ordenarRespuestas(partida.historialRespuestas, letra)
     if (partida.intentos >= partida.intentosMax) {
       // perdio :(
       return false
@@ -310,17 +301,33 @@ class Ahorcado {
       // gano :)
       true
     } else {
-      siguienteTurno(partida, dibujos, listaPalabras)
+      siguienteTurno(partida, dibujos, listaPalabras, turno + 1)
     }
   }
 
   def comprobarVictoria(estadoRespuesta: Array[Char]): Boolean = {
-    for (i <- estadoRespuesta) {
+    for (i <- 0 to estadoRespuesta.length - 1) {
       if (estadoRespuesta(i).equals('_')) {
         return false
       }
     }
     true
+  }
+
+  def leerEntrada(): String = {
+    var respuesta = ""
+    var inputOk = true
+    do {
+      inputOk = true
+      respuesta = readLine()
+      val patternLetra: Regex = "[a-zA-ZñÑ]".r
+      if (respuesta.length > 1 || respuesta.length <= 0 ||
+        !patternLetra.pattern.matcher(respuesta).find()) {
+        println("mas de una letra")
+        inputOk = false
+      }
+    } while (!inputOk)
+    respuesta
   }
 
   def repetir(): Boolean = {
@@ -333,7 +340,7 @@ class Ahorcado {
       !respuesta.equals("n") &&
       !respuesta.equals("N"))
     if (respuesta.equals("Y") || respuesta.equals("y")) {
-      true
+      return true
     }
     false
   }
